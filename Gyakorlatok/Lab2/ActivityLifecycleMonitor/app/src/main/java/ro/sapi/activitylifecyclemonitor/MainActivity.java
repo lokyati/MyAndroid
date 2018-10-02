c308287b6434061110f3110b7870cc13a72f8239
lokodieattil <lokyati15@gmail.com>
2018-10-02 10:57:50 +0300

    Elso commit.

--- Gyakorlatok/Lab2/ActivityLifecycleMonitor/app/src/main/java/ro/sapi/activitylifecyclemonitor/MainActivity.java
+++ Gyakorlatok/Lab2/ActivityLifecycleMonitor/app/src/main/java/ro/sapi/activitylifecyclemonitor/MainActivity.java
@@ -1,49 +1,69 @@
 package ro.sapi.activitylifecyclemonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "Status: ";
+    Button btnActTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener((View.OnClickListener) this);
+
+        Log.d(TAG, "MainActivity: onCreate()");
+
+        btnActTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ActivityTwo.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /** Called when the activity is about to become visible. */
     @Override
     protected void onStart() {
         super.onStart();
-        //ToDo
+        Log.d(TAG, "MainActivity: onStart()");
     }
 
     /** Called when the activity has become visible. */
     @Override
     protected void onResume() {
         super.onResume();
-        //ToDo
+        Log.d(TAG, "MainActivity: onSResume()");
     }
 
     /** Called when another activity is taking focus. */
     @Override
     protected void onPause() {
         super.onPause();
-        //ToDo
+        Log.d(TAG, "MainActivity: onSPause()");
     }
 
     /** Called when the activity is no longer visible. */
     @Override
     protected void onStop() {
         super.onStop();
-        //ToDo
+        Log.d(TAG, "MainActivity: onStop()");
     }
 
     /** Called just before the activity is destroyed. */
     @Override
     public void onDestroy() {
         super.onDestroy();
-        //ToDo
+        Log.d(TAG, "MainActivity: onDestroy()");
     }
 
 }
